3749e6b018e7f628ea816063a7fe4877ddcecd41
reynaldnald <163628831+reynaldnald@users.noreply.github.com>
2024-04-21 12:32:57 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,1 +1,30 @@
+#PYTHON DICTIONARY METHOD
+#USERNAME AND PASSWORD AUTHENTICATION 
+user_login = {
+    "person1": "pass1",
+    "person2": "pass2",
+    "person3": "pass3",
+    "person4": "pass4",
+    "person5": "pass5",
+    "person6": "pass6",
+    "person7": "pass7",
+    "person8": "pass8",
+    "person9": "pass9",
+    "person10": "pass10",
+}
 
+def login():
+    username = input("Username: ")
+    password = input("Password: ")
+
+    if username in user_login:
+        if user_login[username] == password:
+            print("You are now logged in.")
+        else:
+            print("Invalid password.")
+    else:
+        print("Invalid username.")
+
+#CALL LOGIN FUNCTION 
+login()
+    
